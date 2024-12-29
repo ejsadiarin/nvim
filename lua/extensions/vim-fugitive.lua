31610a327fca1e01718a93194c48210e5db5e6cc
ejsadiarin <kyube.ejs@proton.me>
2024-12-29 16:36:11 +0800

    chore: cleanup autocmd

--- lua/extensions/vim-fugitive.lua
+++ lua/extensions/vim-fugitive.lua
@@ -1,5 +1,13 @@
 return {
     "tpope/vim-fugitive",
     opts = {},
-    config = function() end,
+    config = function()
+        -- vim.api.nvim_create_autocmd("FileType", {
+        --     vim.api.nvim_create_augroup("FugitiveMappings", { clear = true }),
+        --     pattern = "fugitive",
+        --     callback = function()
+        --         vim.keymap.set("n", "a", , opts?)
+        --     end,
+        -- })
+    end,
 }
