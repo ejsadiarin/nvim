d6eab2099fc9accad4408de1394f0c9934a8e4a7
ejsadiarin <kyube.ejs@proton.me>
2025-01-10 01:43:53 +0800

    feat(extras): add dap support

--- init.lua
+++ init.lua
@@ -29,6 +29,7 @@ require("lazy").setup({
         { import = "lazyvim.plugins.extras.lang.docker" },
         { import = "lazyvim.plugins.extras.lang.typescript" },
         { import = "lazyvim.plugins.extras.lang.tailwind" },
+        { import = "lazyvim.plugins.extras.dap.core" },
         -- optional plugins
         -- { import = "extensions.obsidian" },
         { import = "extensions.tmux-navigator" },
