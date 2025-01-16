7f27dd842c81b35fbaa1d6f3683d3d07be199257
ejsadiarin <kyube.ejs@proton.me>
2025-01-17 01:13:46 +0800

    feat: use snacks' picker for new things

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -261,17 +261,19 @@ return {
     -- in your project and loads them into a browsable list.
     {
         "folke/todo-comments.nvim",
-        cmd = { "TodoTrouble", "TodoTelescope" },
+        -- cmd = { "TodoTrouble", "TodoTelescope" },
         event = "LazyFile",
         opts = {},
-    -- stylua: ignore
-    keys = {
-      { "]t", function() require("todo-comments").jump_next() end, desc = "Next Todo Comment" },
-      { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous Todo Comment" },
-      { "<leader>xt", "<cmd>Trouble todo toggle focus=true<cr>", desc = "Todo (Trouble)" },
-      { "<leader>xT", "<cmd>Trouble todo toggle focus=true filter = {tag = {TODO,FIX,FIXME}}<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
-      { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
-      { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
-    },
+        -- stylua: ignore
+        keys = {
+          { "]t", function() require("todo-comments").jump_next() end, desc = "Next Todo Comment" },
+          { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous Todo Comment" },
+          { "<leader>xt", "<cmd>Trouble todo toggle focus=true<cr>", desc = "Todo (Trouble)" },
+          { "<leader>xT", "<cmd>Trouble todo toggle focus=true filter = {tag = {TODO,FIX,FIXME}}<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
+          -- { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
+          -- { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
+          { "<leader>st", function() Snacks.picker.todo_comments() end, desc = "Todo" },
+          { "<leader>sT", function () Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } }) end, desc = "Todo/Fix/Fixme" },
+        },
     },
 }
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,3 +1,13 @@
+-- local banned_messages = { "No information available" }
+-- vim.notify = function(msg, ...)
+--     for _, banned in ipairs(banned_messages) do
+--         if msg == banned then
+--             return
+--         end
+--     end
+--     return require("snacks.notifier")(msg, ...)
+-- end
+
 return {
     "folke/snacks.nvim",
     priority = 1000,
@@ -9,6 +19,7 @@ return {
         scope = { enabled = true },
         scroll = { enabled = true },
         bigfile = { enabled = true },
+        profiler = { enabled = true },
         dashboard = {
             preset = {
                 --                header = [[
@@ -47,6 +58,9 @@ return {
         statuscolumn = { enabled = false }, -- we set this in options.lua
         toggle = { map = LazyVim.safe_keymap_set },
         words = { enabled = true },
+        picker = {
+            enabled = true,
+        },
         styles = {
             notification = {
                 wo = { wrap = true }, -- Wrap notifications
@@ -69,119 +83,27 @@ return {
         },
     },
     keys = {
+        -- stylua: ignore start
         { "<leader>n", false },
-        {
-            "<leader>cs",
-            function()
-                Snacks.scratch()
-            end,
-            desc = "Spawn Scratch Buffer",
-        },
-        {
-            "<leader>cS",
-            function()
-                Snacks.scratch.select()
-            end,
-            desc = "Select Scratch Buffer",
-        },
-        {
-            "<leader>un",
-            function()
-                Snacks.notifier.hide()
-            end,
-            desc = "Dismiss All Notifications",
-        },
+        { "<leader>cs", function() Snacks.scratch() end, desc = "Spawn Scratch Buffer", },
+        { "<leader>cS", function() Snacks.scratch.select() end, desc = "Select Scratch Buffer", },
+        { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications", },
         { "<leader>bd", "<CMD>:bd<CR>", desc = "Buffer: [d]elete" },
-        -- { "<leader>bd", function() Snacks.bufdelete() end,               desc = "Buffer: [d]elete" },
-        {
-            "<leader>gg",
-            function()
-                Snacks.lazygit()
-            end,
-            desc = "Lazygit",
-        },
-        -- {
-        --     "<leader>gb",
-        --     function()
-        --         Snacks.git.blame_line()
-        --     end,
-        --     desc = "Git Blame Line",
-        -- },
-        {
-            "<leader>gb",
-            "<CMD>Git blame_line<CR>",
-            desc = "Git Browse",
-        },
-        {
-            "<leader>gB",
-            function()
-                Snacks.gitbrowse()
-            end,
-            desc = "Git Browse",
-        },
-        {
-            "<leader>gf",
-            function()
-                Snacks.lazygit.log_file()
-            end,
-            desc = "Lazygit Current File History",
-        },
-        {
-            "<leader>gl",
-            function()
-                Snacks.lazygit.log()
-            end,
-            desc = "Lazygit Log (cwd)",
-        },
-        {
-            "<leader>cR",
-            function()
-                Snacks.rename()
-            end,
-            desc = "Rename File",
-        },
-        {
-            "<leader>nh",
-            function()
-                Snacks.notifier.show_history()
-            end,
-            desc = "Notification [h]istory",
-        },
-        {
-            "<leader>nd",
-            function()
-                Snacks.notifier.hide()
-            end,
-            desc = "Dismiss All Notifications",
-        },
-        {
-            "<c-/>",
-            function()
-                Snacks.terminal()
-            end,
-            desc = "Toggle Terminal",
-        },
-        {
-            "<c-_>",
-            function()
-                Snacks.terminal()
-            end,
-            desc = "which_key_ignore",
-        },
-        {
-            "]]",
-            function()
-                Snacks.words.jump(vim.v.count1)
-            end,
-            desc = "Next Reference",
-        },
-        {
-            "[[",
-            function()
-                Snacks.words.jump(-vim.v.count1)
-            end,
-            desc = "Prev Reference",
-        },
+        -- { "<leader>bd", function() Snacks.bufdelete() end, desc = "Buffer: [d]elete" },
+        { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit", },
+        -- { "<leader>gb", function() Snacks.git.blame_line() end, desc = "Git Blame Line", },
+        { "<leader>gb", "<CMD>Git blame_line<CR>", desc = "Git Browse", },
+        { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse", },
+        { "<leader>gf", function() Snacks.lazygit.log_file() end, desc = "Lazygit Current File History", },
+        { "<leader>gl", function() Snacks.lazygit.log() end, desc = "Lazygit Log (cwd)", },
+        { "<leader>cR", function() Snacks.rename() end, desc = "Rename File", },
+        { "<leader>nh", function() Snacks.notifier.show_history() end, desc = "Notification [h]istory", },
+        { "<leader>nd", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications", },
+        { "<c-/>", function() Snacks.terminal() end, desc = "Toggle Terminal", },
+        { "<c-_>", function() Snacks.terminal() end, desc = "which_key_ignore", },
+        { "]]", function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", },
+        { "[[", function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", },
+        -- stylua: ignore end
         {
             "<leader>j",
             function()
@@ -225,6 +147,116 @@ return {
                 })
             end,
         },
+        -- Snacks picker keymaps start
+        -- stylua: ignore start
+        -- {
+        --     "<leader>,",
+        --     "<cmd>FzfLua buffers sort_mru=true sort_lastused=true<cr>",
+        --     desc = "Switch Buffer",
+        -- },
+        { "<leader>,", false },
+        { "<leader>ff", false },
+        { "<leader>fF", false },
+        { "<leader>/", false },
+        { "<leader>:", false },
+        { "<leader><space>", false },
+        -- { "<leader>/", LazyVim.pick("live_grep"), desc = "Grep (Root Dir)" },
+        -- { "<leader><space>", LazyVim.pick("files"), desc = "Find Files (Root Dir)" },
+        { "<leader>f:", function() Snacks.picker.command_history() end, desc = "[:]Command History" },
+        -- find
+        { "<leader>fb", function() Snacks.picker.buffers() end, desc = "[b]uffers" },
+        { "<leader>fc", LazyVim.pick.config_files(), desc = "Find [c]onfig File" },
+        { "<leader>fd", LazyVim.pick("files"), desc = "Find [d]irectory Files (Root Dir)" },
+        { "<leader>fe", LazyVim.pick("files", { root = false }), desc = "Find [e]very File (cwd)" },
+        { "<leader>fg", function() Snacks.picker.git_files() end, desc = "Find [g]it Files (git-files)" },
+        -- { "<leader>fq", "<cmd>FzfLua quickfix_stack<cr>", desc = "[q]uickfix stack" },
+        { "<leader>fr", LazyVim.pick("oldfiles"), desc = "[r]ecent Files" },
+        { "<leader>fR", LazyVim.pick("oldfiles", { filter = { cwd = true }}), desc = "[R]ecent Files (cwd)" },
+        -- git
+        { "<leader>gc", function() Snacks.picker.git_log() end, desc = "Git Log" },
+        -- { "<leader>gc", "<cmd>FzfLua git_commits<CR>", desc = "[c]ommits" },
+        { "<leader>gd", function() Snacks.picker.git_diff() end, desc = "[d]iff (hunks)" },
+        { "<leader>gs", function() Snacks.picker.git_status() end, desc = "[s]tatus" },
+        -- grep
+        { "<leader>sg", LazyVim.pick("live_grep"), desc = "[g]rep (Root Dir)" },
+        { "<leader>sG", LazyVim.pick("live_grep", { root = false }), desc = "[G]rep (cwd)" },
+        { "<leader>sw", LazyVim.pick("grep_word"), desc = "Visual selection or [w]ord (Root Dir)", mode = { "n", "x" } },
+        { "<leader>sW", LazyVim.pick("grep_word", { root = false }), desc = "Visual selection or [W]ord (cwd)", mode = { "n", "x" } },
+        -- search
+        { '<leader>s"', function() Snacks.picker.registers() end, desc = "registers" },
+        { "<leader>sa", function() Snacks.picker.autocmds() end, desc = "[a]utocmds" },
+        { "<leader>sc", function() Snacks.picker.command_history() end, desc = "[c]ommand History" },
+        { "<leader>sC", function() Snacks.picker.commands() end, desc = "[C]ommands" },
+        { "<leader>sd", function() Snacks.picker.diagnostics() end, desc = "[d]iagnostics" },
+        { "<leader>sh", function() Snacks.picker.help() end, desc = "[h]elp Pages" },
+        { "<leader>sH", function() Snacks.picker.highlights() end, desc = "[H]ighlights" },
+        { "<leader>sj", function() Snacks.picker.jumps() end, desc = "[j]umps" },
+        { "<leader>sk", function() Snacks.picker.keymaps() end, desc = "[k]eymaps" },
+        { "<leader>sl", function() Snacks.picker.loclist() end, desc = "[l]ocation List" },
+        { "<leader>sM", function() Snacks.picker.man() end, desc = "[M]an Pages" },
+        { "<leader>sm", function() Snacks.picker.marks() end, desc = "[m]arks" },
+        { "<leader>sR", function() Snacks.picker.resume() end, desc = "[R]esume" },
+        { "<leader>sq", function() Snacks.picker.qflist() end, desc = "[q]uickfix List" },
+        { "<leader>uC", function() Snacks.picker.colorschemes() end, desc = "[C]olorschemes" },
+        { "<leader>fp", function() Snacks.picker.projects() end, desc = "[p]rojects" },
+        { "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "Goto [s]ymbol", has = "documentSymbol" },
+        {
+            "<leader>sS",
+            function()
+                require("fzf-lua").lsp_live_workspace_symbols({
+                    regex_filter = symbols_filter,
+                })
+            end,
+            desc = "Goto [S]ymbol (Workspace)",
+        },
+        {
+            "<leader>fl",
+            function()
+                local path = vim.fn.stdpath("data") .. "/lazy/LazyVim"
+                if vim.uv.fs_stat(path) then
+                    Snacks.picker.files({
+                        cwd = path,
+                    })
+                    -- require("fzf-lua").files({
+                    --     cwd = path,
+                    -- })
+                end
+            end,
+            desc = "Find [l]azy files",
+        },
+        {
+            "<leader>sl",
+            function()
+                local path = vim.fn.stdpath("data") .. "/lazy/LazyVim"
+                if vim.uv.fs_stat(path) then
+                    Snacks.picker.grep({
+                        cwd = path
+                    })
+                    -- require("fzf-lua").live_grep_glob({
+                    --     cwd = path,
+                    -- })
+                end
+            end,
+            desc = "Grep from [l]azy files",
+        },
+        {
+            "<leader>ft",
+            function()
+                -- NOTE: requires env variable $VAULT (`export VAULT=...` in your .bashrc or .zshrc)
+                local todos = vim.fn.expand("$VAULT/Personal/todos")
+                if vim.uv.fs_stat(todos) then
+                    Snacks.picker.files({
+                        cwd = todos,
+                    })
+                    -- require("fzf-lua").files({
+                    --     cwd = todos,
+                    -- })
+                end
+            end,
+            desc = "Find [t]odos",
+        },
+        -- stylua: ignore end
+        -- Snacks picker keymaps end
     },
     init = function()
         vim.api.nvim_create_autocmd("User", {
@@ -257,19 +289,19 @@ return {
         })
 
         -- -- Simple LSP Progress
-        -- vim.api.nvim_create_autocmd("LspProgress", {
-        --     ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
-        --     callback = function(ev)
-        --         local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
-        --         vim.notify(vim.lsp.status(), "info", {
-        --             id = "lsp_progress",
-        --             title = "LSP Progress",
-        --             opts = function(notif)
-        --                 notif.icon = ev.data.params.value.kind == "end" and " "
-        --                     or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
-        --             end,
-        --         })
-        --     end,
-        -- })
+        vim.api.nvim_create_autocmd("LspProgress", {
+            ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+            callback = function(ev)
+                local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+                vim.notify(vim.lsp.status(), "info", {
+                    id = "lsp_progress",
+                    title = "LSP Progress",
+                    opts = function(notif)
+                        notif.icon = ev.data.params.value.kind == "end" and " "
+                            or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+                    end,
+                })
+            end,
+        })
     end,
 }
