e444ee4c34e0a3cd1576c649f3637f8026a52a00
ejsadiarin <kyube.ejs@proton.me>
2025-01-22 06:42:58 +0800

    chore: update snacks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -9,28 +9,29 @@
 -- end
 
 return {
-    "folke/snacks.nvim",
-    priority = 1000,
-    lazy = false,
-    ---@type snacks.Config
-    opts = {
-        indent = { enabled = false },
-        input = { enabled = false },
-        scope = { enabled = true },
-        scroll = { enabled = true },
-        bigfile = { enabled = true },
-        profiler = { enabled = true },
-        dashboard = {
-            preset = {
-                --                header = [[
-                --        ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗          Z
-                --        ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║      Z
-                --        ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║   z
-                --        ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║ z
-                --        ███████╗██║  ██║███████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║
-                --        ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝
-                -- ]],
-                header = [[
+    {
+        "folke/snacks.nvim",
+        priority = 1000,
+        lazy = false,
+        ---@type snacks.Config
+        opts = {
+            indent = { enabled = false },
+            input = { enabled = false },
+            scope = { enabled = true },
+            scroll = { enabled = true },
+            bigfile = { enabled = true },
+            profiler = { enabled = true },
+            dashboard = {
+                preset = {
+                    --                header = [[
+                    --        ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗          Z
+                    --        ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║      Z
+                    --        ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║   z
+                    --        ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║ z
+                    --        ███████╗██║  ██║███████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║
+                    --        ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝
+                    -- ]],
+                    header = [[
                      fok
 ]],
         -- stylua: ignore
@@ -42,47 +43,47 @@ return {
           { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
           { icon = " ", key = "q", desc = "Quit", action = ":qa" },
         },
+                },
             },
-        },
-        notifier = {
-            enabled = true,
-            timeout = 3000,
-            style = "fancy",
-        },
-        quickfile = { enabled = true },
-        scratch = {
-            enabled = true,
-            ft = "markdown",
-            icon = nil,
-        },
-        statuscolumn = { enabled = false }, -- we set this in options.lua
-        toggle = { map = LazyVim.safe_keymap_set },
-        words = { enabled = true },
-        picker = {
-            enabled = true,
-        },
-        styles = {
-            notification = {
-                wo = { wrap = true }, -- Wrap notifications
-                relative = "editor",
+            notifier = {
+                enabled = true,
+                timeout = 3000,
+                style = "fancy",
             },
+            quickfile = { enabled = true },
             scratch = {
-                relative = "editor",
-                width = 70,
-                height = 20,
-                bo = { buftype = "", buflisted = false, bufhidden = "hide", swapfile = false },
-                minimal = true,
-                noautocmd = false,
-                -- position = "right",
-                zindex = 20,
-                wo = { winhighlight = "NormalFloat:Normal" },
-                border = "rounded",
-                title_pos = "center",
-                footer_pos = "center",
+                enabled = true,
+                ft = "markdown",
+                icon = nil,
+            },
+            statuscolumn = { enabled = false }, -- we set this in options.lua
+            toggle = { map = LazyVim.safe_keymap_set },
+            words = { enabled = true },
+            picker = {
+                enabled = true,
+            },
+            styles = {
+                notification = {
+                    wo = { wrap = true }, -- Wrap notifications
+                    relative = "editor",
+                },
+                scratch = {
+                    relative = "editor",
+                    width = 70,
+                    height = 20,
+                    bo = { buftype = "", buflisted = false, bufhidden = "hide", swapfile = false },
+                    minimal = true,
+                    noautocmd = false,
+                    -- position = "right",
+                    zindex = 20,
+                    wo = { winhighlight = "NormalFloat:Normal" },
+                    border = "rounded",
+                    title_pos = "center",
+                    footer_pos = "center",
+                },
             },
         },
-    },
-    keys = {
+        keys = {
         -- stylua: ignore start
         { "<leader>n", false },
         { "<leader>cs", function() Snacks.scratch() end, desc = "Spawn Scratch Buffer", },
@@ -103,50 +104,50 @@ return {
         { "<c-_>", function() Snacks.terminal() end, desc = "which_key_ignore", },
         { "]]", function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", },
         { "[[", function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", },
-        -- stylua: ignore end
-        {
-            "<leader>j",
-            function()
-                if vim.wo.diff then
-                    vim.cmd.normal({ "[c", bang = true })
-                else
-                    require("gitsigns").nav_hunk("prev")
-                end
-                -- Snacks.words.jump(-vim.v.count1)
-            end,
-            desc = "Prev git change (hunk)",
-        },
-        {
-            "<leader>k",
-            function()
-                if vim.wo.diff then
-                    vim.cmd.normal({ "]c", bang = true })
-                else
-                    require("gitsigns").nav_hunk("next")
-                end
-                -- Snacks.words.jump(-vim.v.count1)
-            end,
-            desc = "Next git change (hunk)",
-        },
-        {
-            "<leader>N",
-            desc = "Neovim News",
-            function()
-                Snacks.win({
-                    file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
-                    width = 0.6,
-                    height = 0.6,
-                    border = "rounded",
-                    wo = {
-                        spell = false,
-                        wrap = false,
-                        signcolumn = "yes",
-                        statuscolumn = " ",
-                        conceallevel = 3,
-                    },
-                })
-            end,
-        },
+            -- stylua: ignore end
+            {
+                "<leader>j",
+                function()
+                    if vim.wo.diff then
+                        vim.cmd.normal({ "[c", bang = true })
+                    else
+                        require("gitsigns").nav_hunk("prev")
+                    end
+                    -- Snacks.words.jump(-vim.v.count1)
+                end,
+                desc = "Prev git change (hunk)",
+            },
+            {
+                "<leader>k",
+                function()
+                    if vim.wo.diff then
+                        vim.cmd.normal({ "]c", bang = true })
+                    else
+                        require("gitsigns").nav_hunk("next")
+                    end
+                    -- Snacks.words.jump(-vim.v.count1)
+                end,
+                desc = "Next git change (hunk)",
+            },
+            {
+                "<leader>N",
+                desc = "Neovim News",
+                function()
+                    Snacks.win({
+                        file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
+                        width = 0.6,
+                        height = 0.6,
+                        border = "rounded",
+                        wo = {
+                            spell = false,
+                            wrap = false,
+                            signcolumn = "yes",
+                            statuscolumn = " ",
+                            conceallevel = 3,
+                        },
+                    })
+                end,
+            },
         -- Snacks picker keymaps start
         -- stylua: ignore start
         -- {
@@ -198,7 +199,8 @@ return {
         { "<leader>sq", function() Snacks.picker.qflist() end, desc = "[q]uickfix List" },
         { "<leader>uC", function() Snacks.picker.colorschemes() end, desc = "[C]olorschemes" },
         { "<leader>fp", function() Snacks.picker.projects() end, desc = "[p]rojects" },
-        { "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "Goto [s]ymbol"},
+        { "<leader>ss", function() Snacks.picker.lsp_symbols({ filter = LazyVim.config.kind_filter }) end, desc = "Goto [s]ymbol"},
+        { "<leader>sS", function() Snacks.picker.lsp_workspace_symbols({ filter = LazyVim.config.kind_filter }) end, desc = "LSP Workspace Symbols"},
         -- {
         --     "<leader>sS",
         --     function()
@@ -254,53 +256,82 @@ return {
             end,
             desc = "Find [t]odos",
         },
-        -- stylua: ignore end
-        -- Snacks picker keymaps end
+            -- stylua: ignore end
+            -- Snacks picker keymaps end
+        },
+        init = function()
+            vim.api.nvim_create_autocmd("User", {
+                pattern = "VeryLazy",
+                callback = function()
+                    -- Setup some globals for debugging (lazy-loaded)
+                    _G.dd = function(...)
+                        Snacks.debug.inspect(...)
+                    end
+                    _G.bt = function()
+                        Snacks.debug.backtrace()
+                    end
+                    vim.print = _G.dd -- Override print to use snacks for `:=` command
+
+                    -- Create some toggle mappings
+                    Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
+                    Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
+                    Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
+                    Snacks.toggle.diagnostics():map("<leader>ud")
+                    Snacks.toggle.line_number():map("<leader>ul")
+                    Snacks.toggle
+                        .option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
+                        :map("<leader>uc")
+                    Snacks.toggle.treesitter():map("<leader>uT")
+                    Snacks.toggle
+                        .option("background", { off = "light", on = "dark", name = "Dark Background" })
+                        :map("<leader>ub")
+                    Snacks.toggle.inlay_hints():map("<leader>uh")
+                end,
+            })
+
+            -- -- Simple LSP Progress
+            vim.api.nvim_create_autocmd("LspProgress", {
+                ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+                callback = function(ev)
+                    local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+                    vim.notify(vim.lsp.status(), "info", {
+                        id = "lsp_progress",
+                        title = "LSP Progress",
+                        opts = function(notif)
+                            notif.icon = ev.data.params.value.kind == "end" and " "
+                                or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+                        end,
+                    })
+                end,
+            })
+        end,
     },
-    init = function()
-        vim.api.nvim_create_autocmd("User", {
-            pattern = "VeryLazy",
-            callback = function()
-                -- Setup some globals for debugging (lazy-loaded)
-                _G.dd = function(...)
-                    Snacks.debug.inspect(...)
-                end
-                _G.bt = function()
-                    Snacks.debug.backtrace()
-                end
-                vim.print = _G.dd -- Override print to use snacks for `:=` command
 
-                -- Create some toggle mappings
-                Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
-                Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
-                Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
-                Snacks.toggle.diagnostics():map("<leader>ud")
-                Snacks.toggle.line_number():map("<leader>ul")
-                Snacks.toggle
-                    .option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
-                    :map("<leader>uc")
-                Snacks.toggle.treesitter():map("<leader>uT")
-                Snacks.toggle
-                    .option("background", { off = "light", on = "dark", name = "Dark Background" })
-                    :map("<leader>ub")
-                Snacks.toggle.inlay_hints():map("<leader>uh")
-            end,
-        })
-
-        -- -- Simple LSP Progress
-        vim.api.nvim_create_autocmd("LspProgress", {
-            ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
-            callback = function(ev)
-                local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
-                vim.notify(vim.lsp.status(), "info", {
-                    id = "lsp_progress",
-                    title = "LSP Progress",
-                    opts = function(notif)
-                        notif.icon = ev.data.params.value.kind == "end" and " "
-                            or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
-                    end,
-                })
-            end,
-        })
-    end,
+    {
+        "neovim/nvim-lspconfig",
+        opts = function()
+            if LazyVim.pick.want() ~= "snacks" then
+                return
+            end
+            local Keys = require("lazyvim.plugins.lsp.keymaps").get()
+      -- stylua: ignore
+      vim.list_extend(Keys, {
+        { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition", has = "definition" },
+        { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
+        { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
+        { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
+        { "<leader>ss", function() Snacks.picker.lsp_symbols({ filter = LazyVim.config.kind_filter }) end, desc = "LSP Symbols", has = "documentSymbol" },
+        { "<leader>sS", function() Snacks.picker.lsp_workspace_symbols({ filter = LazyVim.config.kind_filter }) end, desc = "LSP Workspace Symbols", has = "workspace/symbols" },
+      })
+        end,
+    },
+    {
+        "folke/todo-comments.nvim",
+        optional = true,
+    -- stylua: ignore
+    keys = {
+      { "<leader>st", function() Snacks.picker.todo_comments() end, desc = "Todo" },
+      { "<leader>sT", function () Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } }) end, desc = "Todo/Fix/Fixme" },
+    },
+    },
 }
