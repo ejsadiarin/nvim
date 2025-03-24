9ce419c374e4e53c034efe7b57f20bf6aa7bd4d7
ejsadiarin <ejs@ejsadiarin.com>
2025-03-25 01:54:13 +0800

    feat: add presence and diffview

--- init.lua
+++ init.lua
@@ -44,6 +44,7 @@ require("lazy").setup({
         { import = "extensions.colorizer" },
         { import = "extensions.rest" }, -- NOTE: requires luarocks and lua-5.1/lua5.1 (compat-lua) installed on machine
         { import = "extensions.presence" },
+        { import = "extensions.diffview" },
         -- { import = "extensions.kulala" },
         -- { import = "extensions.telescope" },
         -- { import = "extensions.fzf-lua" },
--- lua/extensions/presence.lua
+++ lua/extensions/presence.lua
@@ -15,13 +15,13 @@ return {
         file_assets = {}, -- Custom file asset definitions keyed by file names and extensions (see default config at `lua/presence/file_assets.lua` for reference)
         show_time = true, -- Show the timer
 
-        -- Rich Presence text options
-        editing_text = "Editing %s", -- Format string rendered when an editable file is loaded in the buffer (either string or function(filename: string): string)
-        file_explorer_text = "Browsing %s", -- Format string rendered when browsing a file explorer (either string or function(file_explorer_name: string): string)
+        -- Rich Presence text options (use %s for the dynamic placeholder value, ex: editing_text = "Editing %s")
+        editing_text = "Editing some shi", -- Format string rendered when an editable file is loaded in the buffer (either string or function(filename: string): string)
+        file_explorer_text = "Browsing shi", -- Format string rendered when browsing a file explorer (either string or function(file_explorer_name: string): string)
         git_commit_text = "Committing changes", -- Format string rendered when committing changes in git (either string or function(filename: string): string)
         plugin_manager_text = "Managing plugins", -- Format string rendered when managing plugins (either string or function(plugin_manager_name: string): string)
-        reading_text = "Reading %s", -- Format string rendered when a read-only or unmodifiable file is loaded in the buffer (either string or function(filename: string): string)
-        workspace_text = "Working on %s", -- Format string rendered when in a git repository (either string or function(project_name: string|nil, filename: string): string)
+        reading_text = "Reading some shi", -- Format string rendered when a read-only or unmodifiable file is loaded in the buffer (either string or function(filename: string): string)
+        workspace_text = "Working on some secret shi", -- Format string rendered when in a git repository (either string or function(project_name: string|nil, filename: string): string)
         line_number_text = "Line %s out of %s", -- Format string rendered when `enable_line_number` is set to true (either string or function(line_number: number, line_count: number): string)
     },
 }
