77503fd8060a4254324f2ce6d3b8532783e2e12f
ejsadiarin <ejs@ejsadiarin.com>
2025-05-08 22:11:55 +0800

    fix: yaml indent issues

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -24,7 +24,10 @@ return {
         ---@diagnostic disable-next-line: missing-fields
         opts = {
             highlight = { enable = true },
-            indent = { enable = true },
+            indent = {
+                enable = true,
+                disable = { "yaml" },
+            },
             ensure_installed = {
                 "bash",
                 "c",
