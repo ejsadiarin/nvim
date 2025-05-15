f15acd200ad137d1fb5f035f30852020538cd186
ejsadiarin <ejs@ejsadiarin.com>
2025-05-15 17:19:04 +0800

    chore: enable global keymaps

--- lua/extensions/kulala.lua
+++ lua/extensions/kulala.lua
@@ -1,15 +1,15 @@
 return {
     {
         "mistweaverco/kulala.nvim",
+        ft = { "http", "rest" },
+        opts = {
+            -- your configuration comes here
+            global_keymaps = true,
+        },
         keys = {
             { "<leader>Rs", desc = "Send request" },
             { "<leader>Ra", desc = "Send all requests" },
             { "<leader>Rb", desc = "Open scratchpad" },
-        },
-        ft = { "http", "rest" },
-        opts = {
-            -- your configuration comes here
-            global_keymaps = false,
         },
     },
 }
